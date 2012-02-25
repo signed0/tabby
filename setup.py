1483b83c873cf9b7f4f31faea264a7955b07883d
Nathan Villaescusa <theambiguity@gmail.com>
2012-02-25 18:25:22 -0500

    Cleanup

--- setup.py
+++ setup.py
@@ -5,16 +5,18 @@ from setuptools import setup
 
 setup(
     name = 'Tabby',
-    packages = [],
     version = '0.1',
-
+    packages = ['tabby'],
     author = 'Nathan Villaescusa',
     author_email = 'nathan@signedzero.com',
-    description = 'Reads tabular data and turns it into python objects',
-    keywords = 'csv tsv',
+    description = 'Reads tabular data and turns it into python dicts',
+    keywords = 'csv tsv tabular',
     url = 'http://github.com/signed0/tabby',
     license = 'BSD',
     entry_points = {},
-    classifiers = [],
+    classifiers = ['Development Status :: 3 - Alpha',
+                   'Operating System :: OS Independent'
+                   ],
     install_requires = [],
+    zip_safe=True,
 )
