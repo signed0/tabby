63d640253b64c16d31e5667be6fb023205739935
Nathan Villaescusa <nathan@transloc.com>
2012-03-07 14:59:23 -0500

    Increase version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name = 'Tabby',
-    version = '0.1',
+    version = '0.2',
     packages = ['tabby'],
     author = 'Nathan Villaescusa',
     author_email = 'nathan@signedzero.com',
